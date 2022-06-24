e1ee9bfd052355bd9aae47f91e34441fe4e87d17
ringer <ringer@cip.ifi.lmu.de>
2022-06-25 01:52:29 +0200

    It's now possible to start JavaFX Applications without VM Options if you add them to the module-info.java file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module desperate.drosseln.hp {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires org.json;
+    requires javafx.controls;
+
+    opens client_application to javafx.fxml;
+    opens server_application to javafx.fxml;
+    exports client_application;
+    exports server_application;
+}
