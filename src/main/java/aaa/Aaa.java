f57ad56ac145924e822b41bc3c0ea7415023404a
Isabel <muhm@cip.ifi.lmu.de>
2022-07-04 14:41:40 +0200

    Added and optimized some activateMessage Methods with Qinyi and Vivian

--- src/main/java/aaa/Aaa.java
+++ src/main/java/aaa/Aaa.java
@@ -3,6 +3,7 @@ package aaa;
 import client_package.client_gamelogic.map.ModelLoader;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,13 +13,17 @@ public class Aaa extends Application {
     public void start(Stage stage) throws Exception {
         Stage stage1 = new Stage();
         Bbbb bbbb = new Bbbb();
+        ScrollPane scrollPane = new ScrollPane();
         GridPane gridPane = new GridPane();
         ModelLoader modelLoader = new ModelLoader();
-        Scene scene = new Scene(gridPane, 1280, 720 );
+        scrollPane.setContent(gridPane);
+        Scene scene = new Scene(scrollPane, 1280, 720 );
         stage1.setScene(scene);
         stage1.show();
+        ScrollPane scrollPane1 = (ScrollPane) scene.getRoot();
+        GridPane temp = (GridPane) scrollPane1.getContent();
 
-        bbbb.gpm((GridPane) scene.getRoot(), modelLoader.loadMap("dizzy_highway"));
+        bbbb.gpm(temp, modelLoader.loadMap("dizzy_highway"));
 
     }
 }
