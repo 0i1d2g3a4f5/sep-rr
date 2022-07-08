2c3e223f5982bf408337eac792dc083862a512fe
Qinyi Hui <name@cip.ifi.lmu.de>
2022-07-09 01:27:04 +0200

    connect the selectstartpoint method with the updateGameBoard, i think it should function.

--- src/main/java/client_application/ClientGameBasicController.java
+++ src/main/java/client_application/ClientGameBasicController.java
@@ -4,9 +4,12 @@ import gamelogic.Game;
 import gamelogic.Player;
 import gamelogic.Position;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -45,6 +48,7 @@ public class ClientGameBasicController {
 
     public void updateOwnRegister(GridPane gridPane){
         stackTempProgramming.getChildren().add(gridPane);
+        selectStartPoint(new Group());//TODO:Check if it fonctions, i can't test it because there are some errors
     }
 
 
@@ -66,12 +70,16 @@ public class ClientGameBasicController {
     }*/
 
     //TODO: check if it is correct for players to select a start point
-    public void selectStartPoint(MouseEvent e){
-        Group group = null;
-        int x = GridPane.getColumnIndex(group);
-        int y = GridPane.getRowIndex(group);
-        Position position = new Position(x,y);
-        setStartPoint(position);
+    public void selectStartPoint(Group group){
+        group.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                int x = GridPane.getColumnIndex(group);
+                int y = GridPane.getRowIndex(group);
+                Position position = new Position(x,y);
+                setStartPoint(position);
+            }
+        });
     }
 
     public void setStartPoint(Position position){
