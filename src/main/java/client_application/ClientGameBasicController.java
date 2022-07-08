8123e2d6e4e9b267533b39c91fe4fd83b0ed118f
Qinyi Hui <name@cip.ifi.lmu.de>
2022-07-08 19:30:31 +0200

    add the method for players to choose a start point, but not sure

--- src/main/java/client_application/ClientGameBasicController.java
+++ src/main/java/client_application/ClientGameBasicController.java
@@ -1,13 +1,15 @@
 package client_application;
 
+import gamelogic.Game;
+import gamelogic.Player;
+import gamelogic.Position;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.control.ListView;;
 
 /**
  * @author Sarp Cagin Erdogan
@@ -16,6 +18,8 @@ public class ClientGameBasicController {
 
     public ClientApplication clientApplication;
 
+
+
     @FXML
     private ScrollPane scrollAvailableProgramming;
 
@@ -23,47 +27,19 @@ public class ClientGameBasicController {
     private ScrollPane scrollOtherRegisters;
 
     @FXML
-    private ScrollPane scrollChosenProgramming;
+    private ScrollPane scrollPaneGameBoard;
 
     @FXML
     private StackPane stackTempProgramming;
 
-    /*
-    @FXML
-    private ListView<String> handCardsList;
-    
-    @FXML
-    private ListView<String> progCardsList;
-    */
-
-    @FXML
-    private ScrollPane scrollPaneGameBoard;
 
     @FXML
     void startButton(ActionEvent event) {
-    }
 
-    /* @FXML
-    void confirmCards(ActionEvent event) {
     }
-    
-    @FXML
-    void confirmCardsButton(ActionEvent event) {
-    }
-    */
-
-    public void updateGameBoard(GridPane gridPane){
-        scrollPaneGameBoard.setContent(gridPane);
-    }
-
-    public void updateHandCards(GridPane gridPane){
+    public void updateProgrammingCards(GridPane gridPane){
         scrollAvailableProgramming.setContent(gridPane);
     }
-
-    public void updateProgrammingCards(GridPane gridPane) {
-        scrollChosenProgramming.setContent(gridPane);
-    }
-
     public void updateOtherRegisters(GridPane gridPane){
         scrollOtherRegisters.setContent(gridPane);
     }
@@ -72,7 +48,45 @@ public class ClientGameBasicController {
         stackTempProgramming.getChildren().add(gridPane);
     }
 
-    // if prog phase true show hand cards
+
+
+    public void updateGameBoard(GridPane gridPane){
+        scrollPaneGameBoard.setContent(gridPane);
+    }
+
+    // Set AnchorPane visible at start of programming phase
+        // if progPhase (true) then ...
+    // extract info from startingCoordinates and save
+    // display info in handCardsList
     // have click of setCardButton add it to progCardsList
     // confirm whole list and send to game when clicking confirmCardsButton
+
+    /*@FXML
+    AnchorPane controlCenter() {
+
+    }*/
+
+    //TODO: check if it is correct for players to select a start point
+    public void selectStartPoint(MouseEvent e){
+        Group group = null;
+        int x = GridPane.getColumnIndex(group);
+        int y = GridPane.getRowIndex(group);
+        Position position = new Position(x,y);
+        setStartPoint(position);
+    }
+
+    public void setStartPoint(Position position){
+        for(Player player : Game.getInstance().playerList){
+            if(player.getRobot().getPosition() == position){
+                System.out.println("This Start Point is taken. Please choose another one.");
+            }
+            else{
+                player.getRobot().setPosition(position);
+                System.out.println("The Client: "+player.getClient().getId()+ "takes the Start Point" + position);
+            }
+        }
+    }
+
+
+
 }
