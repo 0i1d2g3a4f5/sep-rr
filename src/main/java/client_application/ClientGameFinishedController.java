c3304bdb6a5fbfb0004436ea89664d32fd603350
Qinyi Hui <name@cip.ifi.lmu.de>
2022-07-09 19:17:57 +0200

    Gamefinished Window and Controller

---
+++ src/main/java/client_application/ClientGameFinishedController.java
@@ -0,0 +1,23 @@
+package client_application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * @author Qinyi
+ */
+public class ClientGameFinishedController {
+    @FXML
+    private Button StartAgain;
+    @FXML
+    private Button Exit;
+
+    void StartAgain(ActionEvent event){
+
+    }
+
+    void Exit(ActionEvent event){
+
+    }
+}
