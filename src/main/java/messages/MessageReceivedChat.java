cd249dee7b1cccb1780cfe8d5d605eb3cdee238d
Sarp Cagin Erdogan <erdogan@cip.ifi.lmu.de>
2022-07-28 12:42:50 +0200

    :(

--- src/main/java/messages/MessageReceivedChat.java
+++ src/main/java/messages/MessageReceivedChat.java
@@ -68,10 +68,36 @@ public class MessageReceivedChat extends Message{
     @Override
     public void activateMessageInFrontend(client_package.Client client) throws IOException, ClientNotFoundException {
             if(this.isPrivate){
-                client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1("[Private] " + this.from + " :: " + this.message)));
+                if(from!=client.getId())  {
+                        if (client.clientFromId(from)==null) {
+                            client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1("[Private] " + this.from + " :: " + this.message)));
+                        } else {
+                            client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1("[Private] " + this.from + " | " + client.clientFromId(from).getName() + " :: " + this.message)));
+                        }
+                    }
+                else{
+                    if (client.getName().equals("")) {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1("[Private] " + this.from + " :: " + this.message)));
+                    } else {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1("[Private] " + this.from + " | " + client.getName() + " :: " + this.message)));
+                    }
+                }
             }
             else{
-                client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1(this.from + " :: " + this.message)));
+                if(from!=client.getId()) {
+                    if (client.clientFromId(from)==null) {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1(this.from + " :: " + this.message)));
+                    } else {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1(this.from + " | " + client.clientFromId(from).getName() + " :: " + this.message)));
+                    }
+                }
+                else {
+                    if (client.getName().equals("")) {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1(this.from + " :: " + this.message)));
+                    } else {
+                        client.getClientApplication().addAndExecuteTask(new Task(TaskType.CHATMESSAGE, new TaskString1(this.from + " | " + client.getName() + " :: " + this.message)));
+                    }
+                }
             }
 
     }
